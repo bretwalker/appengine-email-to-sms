df27f76b63748ba69cb856d3430acbb9577679f5
Bret Walker <bretwalker@gmail.com>
2012-10-18 23:01:59 -0400

    First commit

---
+++ incoming_mail.py
@@ -0,0 +1,123 @@
+import hashlib
+import logging
+import re
+import webapp2
+
+from main import Recipient
+
+from google.appengine.api import mail
+from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
+from twilio.rest import TwilioRestClient
+
+from settings import TWILIO_ACCOUT, TWILIO_TOKEN, TWILIO_NUMBER, FOOTER_STUFF1, FOOTER_STUFF2, FOOTER_STUFF3, AUTHORIZED_DOMAIN, ADMIN_EMAIL
+
+class MailHander(InboundMailHandler):
+    def receive(self, mail_message):
+        sender = mail_message.sender
+        
+        logging.info("Received a message from: " + sender)
+        
+        if not sender.endswith(AUTHORIZED_DOMAIN) and not sender.endswith(AUTHORIZED_DOMAIN + '>') and not sender.endswith(ADMIN_EMAIL + '>') and sender != ADMIN_EMAIL:
+            logging.info('Unauthorized domain')
+            return
+    
+        r = Recipient.all().filter('email =', sender).get()
+        
+        if r is None:
+            r = Recipient(email=sender)
+            r.put()
+            
+            f = open('response_email.txt')
+            response = f.read()
+            
+            mail.send_mail(sender="TXT Meeting Reminders<hi@txt-meeting.appspotmail.com>",
+                          to=sender,
+                          subject="Meeting reminders via text message",
+                          body=response)
+                          
+            logging.info('Created user and sent instructions')
+        elif hasattr(mail_message, 'subject') and re.match('\d{10}', re.sub("\D", "", mail_message.subject)):
+            r.phone_number = re.sub("\D", "", mail_message.subject)
+            r.put()
+            
+            self.send_sms('+1' + re.sub("\D", "", mail_message.subject), "I'll send you messages for " + r.email + '.  Send me an email with a phone number in the subject to change your number.') 
+            logging.info('Added/updated phone number')
+            
+        elif hasattr(mail_message, 'subject') and re.sub("\W", "", mail_message.subject.lower()) == 'stop':
+            r.delete()
+            
+            mail.send_mail(sender="TXT Meeting Reminders<hi@txt-meeting.appspotmail.com>",
+                          to=sender,
+                          subject="Meeting reminders via text message",
+                          body="You've been unsubscribed and will no longer receive alerts via text message")
+            logging.info('Deleted user')
+        elif r.phone_number:            
+            plaintext_bodies = mail_message.bodies('text/plain')
+            plaintext_body = list(plaintext_bodies)[0][1].decode()
+            
+            m = hashlib.md5()
+            m.update(plaintext_body)
+            hex_digest = m.hexdigest()
+            
+            if r.last_message <> hex_digest:
+                r.last_message = hex_digest
+                r.put()
+                
+                self.send_sms('+1' + r.phone_number, plaintext_body.replace(FOOTER_STUFF1, '').replace(FOOTER_STUFF2, '').replace(FOOTER_STUFF3, '').strip())
+
+                logging.info('Sent SMS reminder')
+            else:
+                logging.info('Ignored duplicate reminder')
+        else:
+            mail.send_mail(sender="TXT Meeting Reminders<hi@txt-meeting.appspotmail.com>",
+                          to=sender,
+                          subject="Meeting reminders via text message",
+                          body="I don't know where to send your reminders. Could you reply to this message with your phone number in the subject line?")
+            logging.info('Sent email asking for phone number')
+                
+    def split_count(self, s, count):
+        """Split string s at count, preserving words, returning list of strings.
+        
+        TODO: Handle long words/strings without spaces (not very relevant to this app)."""        
+        if s <= count:
+            return [s]
+        
+        strings = []
+        current_string_length = 0
+        current_string = []
+        for word in s.split():
+            current_string_length += len(word) + 1
+            
+            if current_string_length <= count:
+                current_string.append(word)
+            
+            else:
+                strings.append(' '.join(current_string))
+                current_string_length = len(word) + 1
+                current_string = []
+                current_string.append(word)
+                
+        if len(current_string) > 0:
+            strings.append(' '.join(current_string))
+
+        return strings
+                
+    def send_sms(self, to, body):
+        """Sends an SMS, spliting it into 160-character messages."""
+        split_body = self.split_count(body, 155)
+        client = TwilioRestClient(TWILIO_ACCOUT, 
+                                  TWILIO_TOKEN)
+                        
+        if len(split_body) == 1:
+            client.sms.messages.create(to=to,
+                                       from_=TWILIO_NUMBER,
+                                       body=split_body[0])
+        else:
+            i = 1
+            for t in split_body:
+                client.sms.messages.create(to=to,
+                                           from_=TWILIO_NUMBER,
+                                           body=t + ' (' + str(i) + '/' + str(len(split_body)) + ')')
+                i += 1
+                                           
+app = webapp2.WSGIApplication([MailHander.mapping()], debug=True)
---
+++ main.py
@@ -0,0 +1,94 @@
+import webapp2
+
+from google.appengine.ext import db
+
+class Recipient(db.Expando):
+    email = db.StringProperty()
+    phone_number = db.StringProperty()
+    last_message = db.StringProperty()
+    
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Who needs GOOD?')
+
+class OutlookInstructions(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("""<pre>
+        ' Sends email containing reminder highlights
+        ' unless reminder is in category "No Email Reminder"
+        ' Edit email address to set to where email is sent
+        Private Sub Application_Reminder(ByVal Item As Object)
+
+          ' Get out of office status
+          Dim bIsOOO As Boolean
+          bIsOOO = False
+          Dim oNS As Outlook.NameSpace
+          Dim oStores As Outlook.Stores
+          Dim oStr As Outlook.Store
+          Dim oPrp As Outlook.PropertyAccessor
+
+          Set oNS = Application.GetNamespace("MAPI")
+          Set oStores = oNS.Stores
+          For Each oStr In oStores
+            If oStr.ExchangeStoreType = olPrimaryExchangeMailbox Then
+              Set oPrp = oStr.PropertyAccessor
+              bIsOOO = oPrp.GetProperty("http://schemas.microsoft.com/mapi/proptag/0x661D000B")
+            End If
+          Next
+
+          ' Do not send if out of office
+          If bIsOOO = False Then
+            ' Do not send if marked "No Email Reminder"
+            If Item.Categories <> "No Email Reminder" Then
+              Dim objMsg As MailItem
+              Set objMsg = Application.CreateItem(olMailItem)
+
+              objMsg.To = "hi@txt-meeting.appspotmail.com"
+              objMsg.Subject = "Reminder: " & Item.Subject
+              objMsg.BodyFormat = olFormatPlain
+
+              ' Code to handle the 4 types of items that can generate reminders
+              Select Case Item.Class
+                 Case olAppointment '26
+                    If DateDiff("d", Item.Start, Item.End) = 0 Then
+                        objMsg.Body = _
+                          "Reminder: " & Item.Subject & vbCrLf & _
+                          DatePart("m", Item.Start) & "/" & DatePart("d", Item.Start) & " -- " & _
+                          Format(Item.Start, "h:mm ampm") & " - " & Format(Item.End, "h:mm ampm") & vbCrLf & _
+                          "Location: " & Item.Location & vbCrLf
+                    Else
+                        objMsg.Body = _
+                          "Reminder: " & Item.Subject & vbCrLf & _
+                          "Start: " & Format(Item.Start, "h:mm ampm mm/dd/yyyy") & vbCrLf & _
+                          "End: " & Format(Item.End, "h:mm ampm mm/dd/yyyy") & vbCrLf & _
+                          "Location: " & Item.Location & vbCrLf
+                    End If
+                 Case olContact '40
+                    objMsg.Body = _
+                      "Reminder: " & Item.Subject & vbCrLf & _
+                      "Contact: " & Item.FullName & vbCrLf & _
+                      "Phone: " & Item.BusinessTelephoneNumber & vbCrLf & _
+                      "Contact Details: " & vbCrLf & Item.Body
+                  Case olMail '43
+                    objMsg.Body = _
+                      "Reminder: " & Item.Subject & vbCrLf & _
+                      "Due: " & Item.FlagDueBy & vbCrLf
+                  Case olTask '48
+                    objMsg.Body = _
+                      "Reminder: " & Item.Subject & vbCrLf & _
+                      "Start: " & Item.StartDate & vbCrLf & _
+                      "End: " & Item.DueDate & vbCrLf
+              End Select
+
+              objMsg.Send
+              Set objMsg = Nothing
+            End If
+          End If
+        End Sub
+</pre>
+        """)
+        
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/outlook-code', OutlookInstructions)
+], debug=False)
